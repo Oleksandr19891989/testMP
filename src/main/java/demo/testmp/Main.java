18d78f4042cbdff39cef9f3b0724ee5de69b291d
OleksandrDarchyk <oledar01@easv365.dk>
2025-01-06 14:56:49 +0100

    main

--- src/main/java/demo/testmp/Main.java
+++ src/main/java/demo/testmp/Main.java
@@ -1,4 +1,5 @@
 package demo.testmp;
 
 public class Main {
+
 }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module demo.testmp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens demo.testmp to javafx.fxml;
-    exports demo.testmp;
-}
